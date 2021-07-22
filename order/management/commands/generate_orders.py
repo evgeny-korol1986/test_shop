a1fe224ec62cb24f6d7ac54ca5cced2224441db1
evgeny-korol1986 <evgeny.korol1986@gmail.com>
2021-07-22 15:16:58 +0300

    Fix generate_orders

--- order/management/commands/generate_orders.py
+++ order/management/commands/generate_orders.py
@@ -13,16 +13,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         num_orders = options.get('num_orders', 5)
         start_date = datetime(year=2018, month=1, day=1, hour=9, tzinfo=pytz.UTC)
-
+        order_items = []
         for num in range(num_orders):
             date = start_date + timedelta(hours=num)
-            order = Order.objects.create(number=num, create_date=date)
             num_items = random.randint(1, 5)
+            order = Order.objects.create(number=num, create_date=date)
             for n in range(num_items):
-                OrderItem.objects.create(
+                order_items.append(OrderItem(
                     order=order,
                     product_name=f'Товар-{random.randint(1, 100)}',
                     product_price=random.randint(100, 9999),
                     amount=random.randint(1, 10),
-                )
+                ))
+        OrderItem.objects.bulk_create(order_items)
         print('Complete!')
